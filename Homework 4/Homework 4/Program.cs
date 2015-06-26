75027fe32afde21912eaf0a47c4aff921bfbb7e6
ppenkov <penkov_law@abv.bg>
2015-06-26 23:42:35 +0300

    Homework 4

---
+++ Homework 4/Homework 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] N = new int [5];
+            for (int i = 0; i < N.Length; i++)
+            {
+                N[i] = int.Parse(Console.ReadLine());
+            }
+                int sum = 0;
+            for (int i = 0; i < N.Length; i++)
+			{
+                if (N[i] % 2 == 0)
+                {
+                    sum = sum + N[i];
+                }
+            } 
+                    Console.WriteLine(sum);
+            }
+        }
+    }
+
---
+++ Homework 4/Task 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the array:");
+            int length = int.Parse(Console.ReadLine());
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int index = Array.IndexOf(array, 5);
+            Console.WriteLine(index);
+        }
+    }
+}
